d10d9fbf7dce3bed8f41225008ab0aa7c44d34c1
TylerTam <46612488+TylerTam@users.noreply.github.com>
2019-10-20 19:40:53 -0400

    Added Aim Assist
    Uses physics prediction to follow target, if they are within a range and angle
    Makes a ui element follow the predicted path.
    
    -If shot directly at predicted position, shot will hit about 70-80% of the time

---
+++ Assets/_MachDeath/Scripts/AimAssist/Gun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public GameObject m_bullet;
+    
+    public Transform m_fireSpot;
+    public float m_bulletSpeed;
+    private bool m_canFire = true;
+    public float m_fireTime = .25f;
+    private void Update()
+    {
+        if (Input.GetAxis("Fire3")>0)
+        {
+            if (m_canFire)
+            {
+                m_canFire = false;
+                FireGun();
+                
+            }
+        }
+        else
+        {
+            m_canFire = true;
+        }
+    }
+
+    private void FireGun()
+    {
+        GameObject bullet = Instantiate(m_bullet);
+        bullet.transform.position = m_fireSpot.transform.position;
+        bullet.transform.rotation = m_fireSpot.rotation;
+        bullet.GetComponent<Rigidbody>().velocity = m_fireSpot.transform.forward * m_bulletSpeed;
+    }
+
+
+}
