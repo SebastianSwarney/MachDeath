a9f91619485c3e54e81e78a84731217c8d991352
TylerTam <46612488+TylerTam@users.noreply.github.com>
2019-10-20 19:57:14 -0400

    Re-Added Aim assist

---
+++ Assets/_MachDeath/Scripts/AimAssist/BulletTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTest : MonoBehaviour
+{
+    public Vector3 m_startingPos;
+    private Rigidbody m_rb;
+    public string m_collideTag = "Finish";
+
+    private void Awake()
+    {
+        m_rb = GetComponent<Rigidbody>();
+        m_startingPos = transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == m_collideTag)
+        {
+            m_rb.velocity = Vector3.zero;
+            m_rb.angularVelocity = Vector3.zero;
+            transform.position = m_startingPos;
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
