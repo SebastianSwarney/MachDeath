ed5efdec9488979df3df42f62abe97721b1df3f6
zhangwu <zhangwu@sheridancollege.ca>
2019-10-22 15:51:08 -0400

    Spear Lerp FeelsGoodMan

--- Assets/_MachDeath/Scripts/WeaponsAndItems/WeaponBase.cs
+++ Assets/_MachDeath/Scripts/WeaponsAndItems/WeaponBase.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponBase : ItemBase
 {
@@ -12,6 +13,19 @@ public class WeaponBase : ItemBase
 
     [SerializeField]
     private float sphereCastSize;
+
+    [SerializeField]
+    private Image Recticle;
+
+    [SerializeField]
+    private float LerpSpeed;
+
+    [SerializeField]
+    private AnimationCurve animationCurve;
+
+    private float LerpTimer;
+
+    private Quaternion defaultSpearOrientation;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +42,8 @@ public class WeaponBase : ItemBase
     {
         //Get reference to fps Camera and WeaponController//Should be item controller
         base.GetData();
+        defaultSpearOrientation = this.transform.localRotation;
+        LerpTimer = 0;
     }
 
     protected override void ApplyUseItem()
@@ -58,20 +74,29 @@ public class WeaponBase : ItemBase
         if (Physics.SphereCast(rayOrigin, sphereCastSize, fpsCam.transform.forward, out hit, Mathf.Infinity, targetLayerMask))
         {
             Debug.Log("I hit " + hit.collider.name);
-            this.transform.LookAt((CalculateShotNoMarker()));
+            this.transform.LookAt(Vector3.Lerp(this.transform.position, (CalculateShotNoMarker()), animationCurve.Evaluate((LerpTimer * LerpSpeed))));
             Debug.DrawLine(transform.position, hit.point, Color.cyan);
+            Recticle.color = Color.cyan;
+            LerpTimer = 0;
             return;
         }
         else
         {
-            this.transform.rotation = this.transform.rotation;
+            LerpTimer += Time.deltaTime;
+            Recticle.color = Color.red;
+            this.transform.localRotation = Quaternion.Lerp(this.transform.localRotation, defaultSpearOrientation, LerpTimer * LerpSpeed);
+            LerpTimer = 0;
         }
     }
 
     private Vector3 CalculateShot()
     {
+
+        //Sphere cast forward to see if the enemy falls within the cast area on targetLayerMask, if so return the Vector3 of the hit point
+        //Using the hitpoint to stear the shot by having the spear container look at the enemy target
         if (Physics.SphereCast(rayOrigin, sphereCastSize, fpsCam.transform.forward, out hit, Mathf.Infinity, targetLayerMask))
         {
+            LerpTimer += Time.deltaTime;
             Debug.Log("I hit " + hit.collider.name);
             OnRayCastHit();
 
@@ -79,14 +104,18 @@ public class WeaponBase : ItemBase
         }
         else
         {
+            //If aim assist is not active, then shoot the spear straight forward
             return (transform.position + this.transform.forward * 100f);
         }
     }
 
     private Vector3 CalculateShotNoMarker()
     {
+        //Sphere cast forward to see if the enemy falls within the cast area on targetLayerMask, if so return the Vector3 of the hit point
+        //Using the hitpoint to stear the shot by having the spear container look at the enemy target
         if (Physics.SphereCast(rayOrigin, sphereCastSize, fpsCam.transform.forward, out hit, Mathf.Infinity, targetLayerMask))
         {
+            LerpTimer += Time.deltaTime;
             Debug.Log("I hit " + hit.collider.name);
             //OnRayCastHit();
 
@@ -94,18 +123,24 @@ public class WeaponBase : ItemBase
         }
         else
         {
+            //If aim assist is not active, then shoot the spear straight forward
             return (transform.position + this.transform.forward * 100f);
         }
     }
 
+    //Draw a red dot where the raycast hits
     private void OnRayCastHit()
     {
         Instantiate(hitMarker, hit.point, Quaternion.identity);
     }
 
+    //Draw The Size spherecast
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, sphereCastSize);
+        for (int i = 0; i < 100; i++)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(transform.position + this.transform.forward * i * 0.5f, sphereCastSize);
+        }
     }
 }
