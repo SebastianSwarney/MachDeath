405bf13651c04124c44ba1d895ff2e051db8b903
TylerTam <46612488+TylerTam@users.noreply.github.com>
2019-10-24 14:30:30 -0400

    Throwing Spears Works on client and server
    Spears no longer shoot sstraight up

--- Assets/_MachDeath/Scripts/WeaponsAndItems/ThrowSpear.cs
+++ Assets/_MachDeath/Scripts/WeaponsAndItems/ThrowSpear.cs
@@ -16,25 +16,31 @@ namespace Mirror.MachDeath
         {
             m_playerProperties = GetComponent<PlayerProperties>();
         }
+
+        public void SpawnSpear()
+        {
+
+            CmdCreateSpear(m_fireSpot.position, m_fireSpot.rotation, m_fireSpot.forward);
+        }
         [Command]
-        public void CmdCreateSpear()
+        public void CmdCreateSpear(Vector3 p_pos, Quaternion p_quat, Vector3 p_dir)
         {
             
             GameObject newSpear = Instantiate(m_spearObject);
-            newSpear.transform.position = m_fireSpot.position;
-            newSpear.transform.rotation = m_fireSpot.rotation;
+            newSpear.transform.position = p_pos;
+            newSpear.transform.rotation = p_quat;
             
             newSpear.GetComponent<ProjectileProperties>().m_spearOwner = m_playerProperties;
             NetworkServer.Spawn(newSpear);
-
-            RpcAssignVelocity(newSpear);
+            newSpear.GetComponent<Rigidbody>().velocity =  p_dir* m_throwSpeed;
+            RpcAssignVelocity(newSpear,p_quat, p_pos, p_dir);
         }
 
         [ClientRpc]
-        private void RpcAssignVelocity(GameObject p_spear)
+        private void RpcAssignVelocity(GameObject p_spear, Quaternion p_quat, Vector3 p_pos, Vector3 p_dir)
         {
             p_spear.GetComponent<ProjectileProperties>().m_spearOwner = m_playerProperties;
-            p_spear.GetComponent<Rigidbody>().velocity = m_fireSpot.forward * m_throwSpeed;
+            p_spear.GetComponent<Rigidbody>().velocity = p_dir * m_throwSpeed;
         }
         
     }
