d65f1ac845a63fb835321bed02951d1fd68632b8
SebastianSwarney <34040389+SebastianSwarney@users.noreply.github.com>
2019-10-24 20:54:04 -0400

    Animations

---
+++ Assets/_MachDeath/Scripts/Players/PlayerGraphicsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGraphicsController : MonoBehaviour
+{
+    private Animator m_animator;
+
+    private PlayerMovementController m_movementController;
+
+    private CharacterController m_charController;
+
+    private void Start()
+    {
+        m_animator = GetComponentInChildren<Animator>();
+        m_movementController = GetComponent<PlayerMovementController>();
+        m_charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Vector3 reletiveVelocity = transform.InverseTransformDirection(m_movementController.m_velocity);
+
+        m_animator.SetFloat("Forward", reletiveVelocity.z);
+        m_animator.SetFloat("Strafe", reletiveVelocity.x);
+    }
+}
